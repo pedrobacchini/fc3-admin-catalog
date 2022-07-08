91837e9a0ae3f8590bb46d3061968859c430955c
pedrobacchini <pedrobacchini@outlook.com>
2022-07-08 00:04:32 -0300

    feat: create category domain base class

--- application/src/main/java/com/github/pedrobacchini/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/github/pedrobacchini/admin/catalog/application/UseCase.java
@@ -1,10 +1,10 @@
 package com.github.pedrobacchini.admin.catalog.application;
 
-import com.github.pedrobacchini.admin.catalog.domain.Category;
+import com.github.pedrobacchini.admin.catalog.domain.category.Category;
 
 public class UseCase {
 
     public Category execute() {
-        return new Category();
+        return Category.newCategory("Filmes", "A categoria mais assistida", true);
     }
 }
--- domain/src/main/java/com/github/pedrobacchini/admin/catalog/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.github.pedrobacchini.admin.catalog.domain;
-
-public class Category {
-    public String id;
-}
