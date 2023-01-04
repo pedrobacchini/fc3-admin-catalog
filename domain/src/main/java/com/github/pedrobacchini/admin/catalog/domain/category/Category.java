4433ea9e06d7ff7e5e0142f4bcae14c2aa259d86
pedrobacchini <pedrobacchini@outlook.com>
2023-01-03 21:31:02 -0300

    feat: category jpa entity

--- domain/src/main/java/com/github/pedrobacchini/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/github/pedrobacchini/admin/catalog/domain/category/Category.java
@@ -41,6 +41,17 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
+    public static Category with(
+        final CategoryID id,
+        final String name,
+        final String description,
+        final boolean active,
+        final Instant createdAt,
+        final Instant updatedAt,
+        final Instant deletedAt) {
+        return new Category(id, name, description, active, createdAt, updatedAt, deletedAt);
+    }
+
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
---
+++ infrastructure/src/main/java/com/github/pedrobacchini/admin/catalog/infrastructure/category/persistence/CategoryJpaEntity.java
@@ -0,0 +1,136 @@
+package com.github.pedrobacchini.admin.catalog.infrastructure.category.persistence;
+
+import com.github.pedrobacchini.admin.catalog.domain.category.Category;
+import com.github.pedrobacchini.admin.catalog.domain.category.CategoryID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+
+@Table
+@Entity
+public class CategoryJpaEntity {
+
+    @Id
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 4000)
+    private String description;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at", columnDefinition = "DATETIME(6)")
+    private Instant deletedAt;
+
+    public CategoryJpaEntity() {
+    }
+
+    private CategoryJpaEntity(
+        final String id,
+        final String name,
+        final String description,
+        final boolean active,
+        final Instant createdAt,
+        final Instant updatedAt,
+        final Instant deletedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static CategoryJpaEntity from(final Category category) {
+        return new CategoryJpaEntity(
+            category.getID().getValue(),
+            category.getName(),
+            category.getDescription(),
+            category.isActive(),
+            category.getCreatedAt(),
+            category.getUpdatedAt(),
+            category.getDeletedAt()
+        );
+    }
+
+    public Category toAggregate() {
+        return Category.with(
+            CategoryID.from(getId()),
+            getName(),
+            getDescription(),
+            isActive(),
+            getCreatedAt(),
+            getUpdatedAt(),
+            getDeletedAt());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(final boolean active) {
+        this.active = active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(final Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(final Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(final Instant deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+}
