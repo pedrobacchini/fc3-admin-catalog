b5c182efea182932ae93f7e0d0691e64200a19fd
pedrobacchini <pedrobacchini@outlook.com>
2023-01-22 13:59:53 -0300

    fix: remove runner used just test use case inject

--- infrastructure/src/main/java/com/github/pedrobacchini/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/github/pedrobacchini/admin/catalog/infrastructure/Main.java
@@ -1,17 +1,9 @@
 package com.github.pedrobacchini.admin.catalog.infrastructure;
 
 
-import com.github.pedrobacchini.admin.catalog.application.category.create.CreateCategoryUseCase;
-import com.github.pedrobacchini.admin.catalog.application.category.delete.DeleteCategoryUseCase;
-import com.github.pedrobacchini.admin.catalog.application.category.retrieve.get.GetCategoryByIdUseCase;
-import com.github.pedrobacchini.admin.catalog.application.category.retrieve.list.ListCategoriesUseCase;
-import com.github.pedrobacchini.admin.catalog.application.category.update.UpdateCategoryUseCase;
 import com.github.pedrobacchini.admin.catalog.infrastructure.configuration.WebServerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -20,17 +12,4 @@ public class Main {
         SpringApplication.run(WebServerConfig.class, args);
     }
 
-
-    @Bean
-    @DependsOnDatabaseInitialization
-    ApplicationRunner runner(
-        CreateCategoryUseCase createCategoryUseCase,
-        UpdateCategoryUseCase updateCategoryUseCase,
-        DeleteCategoryUseCase deleteCategoryUseCase,
-        GetCategoryByIdUseCase getCategoryByIdUseCase,
-        ListCategoriesUseCase listCategoriesUseCase) {
-        return args -> {
-
-        };
-    }
 }
